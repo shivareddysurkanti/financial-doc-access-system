98caaf9c0afd330de4852b5d831f92c75ecfcb64
Shiva Reddy  Surkanti <surkantishivareddy465@gmail.com>
2025-04-27 23:01:31 +0300

    Update AdminUser.java

--- AdminUser.java
+++ AdminUser.java
@@ -1,9 +1,12 @@
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class AdminUser {
 
 	private int adminID;
-	private string username;
-	private string passwordHash;
-	private string role;
+	private String username;
+	private String passwordHash;
+	private String role;
 	private Timestamp createdAt;
 
 }
