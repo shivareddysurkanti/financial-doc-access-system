047024fb6ec0621b4d9f4aad6874c9c267aff0ff
Shiva Reddy  Surkanti <surkantishivareddy465@gmail.com>
2025-04-27 21:43:38 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,8 +1,43 @@
 public class Account {
 
-	private int accountID;
-	private string accountNumber;
-	private int deliveryFrequency;
-	private int deliveryPreference;
+    private int accountID;
+    private String accountNumber;
+    private int deliveryFrequency;
+    private int deliveryPreference;
 
+    // Getter and Setter for accountID
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    // Getter and Setter for accountNumber
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    // Getter and Setter for deliveryFrequency
+    public int getDeliveryFrequency() {
+        return deliveryFrequency;
+    }
+
+    public void setDeliveryFrequency(int deliveryFrequency) {
+        this.deliveryFrequency = deliveryFrequency;
+    }
+
+    // Getter and Setter for deliveryPreference
+    public int getDeliveryPreference() {
+        return deliveryPreference;
+    }
+
+    public void setDeliveryPreference(int deliveryPreference) {
+        this.deliveryPreference = deliveryPreference;
+    }
 }
