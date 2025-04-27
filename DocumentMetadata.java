1b6e50d5045d8e2ba45364bc845c114879387d1a
Shiva Reddy  Surkanti <surkantishivareddy465@gmail.com>
2025-04-27 23:03:25 +0300

    Update DocumentMetadata.java

--- DocumentMetadata.java
+++ DocumentMetadata.java
@@ -1,7 +1,10 @@
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class DocumentMetadata {
 
 	private int documentID;
-	private string docType;
+	private String docType;
 	private Date docDate;
 	private int isViewed;
 	private Timestamp viewedAt;
