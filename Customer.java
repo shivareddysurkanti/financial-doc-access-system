5fee70ecb3fbbba261e625c3c4f239e9a4dd9d74
Shiva Reddy  Surkanti <surkantishivareddy465@gmail.com>
2025-04-27 19:35:23 +0300

    Adding getters/setters to Customer.java

--- Customer.java
+++ Customer.java
@@ -7,4 +7,51 @@ public class Customer {
 	private int isActive;
 	private Timestamp createdAt;
 
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast4SSN() {
+        return last4SSN;
+    }
+
+    public void setLast4SSN(String last4SSN) {
+        this.last4SSN = last4SSN;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
